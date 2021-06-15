e29196dded50f461baba83f05065b1b445d1718c
Danny Meyer <danny@xasopheno.com>
2021-06-15 15:34:09 -0600

    react-create-app on Heroku

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChangeEvent, useState } from "react"
+import "./App.css"
 
 function App() {
+  const [text, setText] = useState("")
+
+  const onChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value)
+  }
+
+  const sayUppercase = () => {
+    const msg = new SpeechSynthesisUtterance()
+    msg.text = "uppercase"
+    window.speechSynthesis.speak(msg)
+  }
+
+  const sayLowerCase = () => {
+    const msg = new SpeechSynthesisUtterance()
+    msg.text = "lowercase"
+    window.speechSynthesis.speak(msg)
+  }
+
+  const is_number = (char: string) => {
+    return !isNaN(parseInt(char))
+  }
+
+  const readText = () => {
+    for (let char of text.split("")) {
+      if (!is_number(char)) {
+        if (char === char.toUpperCase()) {
+          sayUppercase()
+        } else {
+          sayLowerCase()
+        }
+      }
+      const msg = new SpeechSynthesisUtterance()
+      msg.text = char
+      window.speechSynthesis.speak(msg)
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi Mom</h1>
+      <p className="comic">Paste text here:</p>
+      <textarea
+        id="textarea"
+        name="story"
+        rows={3}
+        cols={33}
+        value={text}
+        onChange={onChange}
+      />
+
+      <p className="comic">{text}</p>
+      <button onClick={readText}>
+        <p className="buttonText">Read Text</p>
+      </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
